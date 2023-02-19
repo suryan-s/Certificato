e5ef484b7d59e8b7b8542ffeb85ed3b3da8f9f6f
Suryan S <suryannasa@gmail.com>
2023-02-20 00:39:16 +0530

    MInor update

--- func.py
+++ func.py
@@ -15,9 +15,10 @@ try:
     
 except ImportError:
     client = None
-    from pypandoc.pandoc_download import download_pandoc
-    download_pandoc()
+    # from pypandoc.pandoc_download import download_pandoc
+    # download_pandoc()
     import pypandoc
+    import pypandoc.pandoc_download
     # command = "sudo apt-get install unoconv"
     # os.system(command)
 
