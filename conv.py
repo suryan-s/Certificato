5c4616acd418da7c0a7c0042ba913d7ac97c48a9
Suryan S <suryannasa@gmail.com>
2023-02-20 16:43:24 +0530

    MInor bug fix 13

--- conv.py
+++ conv.py
@@ -4,11 +4,11 @@ import os
 def convert_to_pdf(docx_file, output_dir, name, filename):
     try:
         # command = f"libreoffice --headless --convert-to pdf:writer_pdf_Export {docx_file} --outdir {output_dir} --infilter=\"Microsoft Word 2007-2013 XML\" "
-        command = f"pandoc {docx_file} -o {output_dir}/{filename}.pdf"
+        command = f"pandoc {docx_file} -o {output_dir}/{name}.pdf"
         # Run the command in the shell
         os.system(command)
         # Move the output pdf file to the specified output directory
-        os.rename(f"{output_dir}/{filename}.pdf", f"{output_dir}/{name}.pdf")
+        # os.rename(f"{output_dir}/{filename}.pdf", f"{output_dir}/{name}.pdf")
     except Exception as e:
         print("Error at convert_to_pdf ",e)
 
